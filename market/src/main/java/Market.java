460a058befa82b59df42c7531cbb3385dfc60b81
Elena Bondarenko <elena.bondarenko@worldapp.com>
2020-02-26 17:28:28 +0200

    fix sell option

--- market/src/main/java/Market.java
+++ market/src/main/java/Market.java
@@ -5,11 +5,73 @@ import java.io.*;
 import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 public class Market {
 
-    private static final Map<Integer, String> instruments = null;
+    private static final Map<Integer, String> instruments = new Map<Integer, String>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey( Object key ) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue( Object value ) {
+            return false;
+        }
+
+        @Override
+        public String get( Object key ) {
+            return null;
+        }
+
+        @Override
+        public String put( Integer key, String value ) {
+            return null;
+        }
+
+        @Override
+        public String remove( Object key ) {
+            return null;
+        }
+
+        @Override
+        public void putAll( Map<? extends Integer, ? extends String> m ) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<Integer> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<String> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<Integer, String>> entrySet() {
+            return null;
+        }
+    };
 
     public static void main( String[] args ) {
         try ( Socket clientSocket = new Socket( "localhost", 5001 ) ) {
